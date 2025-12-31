787859a6a8c09fad675a82df5b7ae186c6d4f4ef
Meryem Jirani <meryemjirani9@gmail.com>
2025-12-31 12:13:01 +0100

    modification3

--- tasks.js
+++ tasks.js
@@ -2,6 +2,9 @@ const tasks=[];
 function addTask(task){
     tasks.push(task);
     console.log("task added:", task);
-
+    if(!task){
+        console.log("empty task not allowed");
+        return;
+    }
 }
 MediaSourceHandle.exports={tasks,addTask};
